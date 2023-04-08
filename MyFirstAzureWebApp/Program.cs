e599f8d19cfe4cf628ff91936aa3cff2e2ee674a
Your Name <arslanmanzoor332@gmail.com>
2023-04-08 19:44:33 +0500

    changes

--- MyFirstAzureWebApp/Program.cs
+++ MyFirstAzureWebApp/Program.cs
@@ -8,7 +8,7 @@ string environmentName = null;
 #if DEBUG
 environmentName = "Development";
 #elif RELEASE
-environmentName = "Development";
+environmentName = "Testing";
 #elif TESTING
 environmentName = "Testing";
 #elif STAGING 
